a711ddc9ca81f2aafb5eaf76a124d693d0a93633
Creativityliberty <creativityliberty@gmail.com>
2026-07-17 01:11:44 +0200

    design: remove default starter kit Repository and Documentation footer links from sidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -1,7 +1,6 @@
 import { Link, usePage } from '@inertiajs/react';
 import { BookOpen, CreditCard, FolderGit2, GraduationCap, LayoutGrid } from 'lucide-react';
 import AppLogo from '@/components/app-logo';
-import { NavFooter } from '@/components/nav-footer';
 import { NavMain } from '@/components/nav-main';
 import { NavUser } from '@/components/nav-user';
 import {
@@ -22,18 +21,7 @@ import adminPlans from '@/actions/App/Http/Controllers/Admin/Plans/PlanControlle
 import trainerCourses from '@/actions/App/Http/Controllers/Trainer/Courses/CourseController';
 import studentCourses from '@/actions/App/Http/Controllers/Student/Courses/CourseController';
 
-const footerNavItems: NavItem[] = [
-    {
-        title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
-        icon: FolderGit2,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#react',
-        icon: BookOpen,
-    },
-];
+
 
 export function AppSidebar() {
     const { auth } = usePage().props;
@@ -104,7 +92,6 @@ export function AppSidebar() {
             </SidebarContent>
 
             <SidebarFooter>
-                <NavFooter items={footerNavItems} className="mt-auto" />
                 <NavUser />
             </SidebarFooter>
         </Sidebar>
